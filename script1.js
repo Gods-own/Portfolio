e4b5ee2da17f3d923c38332409ef9a5c002f9162
Idumeka Oritogun <oritogunjemi@gmail.com>
2021-09-20 16:03:55 +0200

    My Portfolio

--- script1.js
+++ script1.js
@@ -1,10 +1,15 @@
+let touchEvent = 'onclick' in window ? 'click' : 'touchstart';
+
 document.addEventListener("DOMContentLoaded", function() {   
 
     var modal = document.getElementById("myModal");
      var modal1 = document.getElementById("myModal1");
     var span = document.getElementsByClassName("close")[0];
-    var contacts = document.querySelectorAll(".contact-me")
-    var resumes = document.querySelectorAll(".myResume")
+    var contacts = document.querySelectorAll(".contact-me");
+    var resumes = document.querySelectorAll(".myResume");
+    var close = document.querySelector(".cv-close-btn");
+    var yes = document.querySelector(".yes-btn");
+    var no = document.querySelector(".no-btn");
 
 
     function contac() {
@@ -14,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
    function myresume() {
     modal1.style.display = "block";
+    document.querySelector(".modal-div").style.display = "block";
+
    }
 
    span.onclick = function() {
@@ -26,12 +33,29 @@ window.onclick = function(event) {
   }
 }
 
-window.onclick = function(event) {
+window.addEventListener(touchEvent, function() {
   if (event.target == modal1) {
-    modal1.style.display = "none";
+    document.querySelector(".modal-div").style.display = "none";   
+    document.querySelector(".modal-question").style.display = "block"; 
   }
-}
+})
 
+close.addEventListener('click', function() {
+    document.querySelector(".modal-div").style.display = "none";    
+     document.querySelector(".modal-question").style.display = "block"; 
+})
+
+yes.addEventListener('click', function() {
+  setTimeout(function() {
+    document.querySelector(".modal-question").style.display = "none"; 
+     modal1.style.display = "none";
+  },2000)
+})
+
+no.addEventListener('click', function() {
+    document.querySelector(".modal-question").style.display = "none"; 
+     modal1.style.display = "none";
+})
 
     contacts.forEach(function(contact){
         contact.addEventListener("click", contac)
@@ -40,6 +64,5 @@ window.onclick = function(event) {
     resumes.forEach(function(resume){
         resume.addEventListener("click", myresume)
     });
-
 
   });
